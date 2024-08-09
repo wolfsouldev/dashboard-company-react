7ec288800654df7b3448ea569bd6b3af0a4ed8aa
wolfsoul01 <alejandrof011021@gmail.com>
2024-08-09 15:56:06 -0400

    style: :art: Add index form ui componetns

---
+++ src/components/ui/index.ts
@@ -0,0 +1,30 @@
+export { Badge } from "./badge";
+export {} from "./breadcrumb";
+export {} from "./button";
+export {} from "./card";
+export {} from "./chart";
+export {} from "./checkbox";
+export {} from "./dropdown-menu";
+export {} from "./input";
+export {} from "./label";
+export {} from "./pagination";
+export {} from "./progress";
+export {} from "./separator";
+export {} from "./sheet";
+export {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./table";
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
