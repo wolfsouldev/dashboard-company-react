f3dab686c9e821e4a8fc12cb149b0f1f1ef3f62b
wolfsoul01 <alejandrof011021@gmail.com>
2024-08-10 21:50:59 -0400

    fix: :lipstick: Fix style layout for vh

--- src/components/container/DashboardContainer.tsx
+++ src/components/container/DashboardContainer.tsx
@@ -5,11 +5,11 @@ import SideBar from "../layout/Sidebar";
 export default function DashboardContainer() {
   return (
     <>
-      <section className="flex min-h-screen   flex-col bg-muted/40">
+      <section className="flex  flex-col bg-muted/40 min-h-screen inset-y-0 ">
         <SideBar />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <HeaderAdmin />
-          <main className=" w-[95vw] px-5">
+          <main className=" px-8  ">
             <Outlet />
           </main>
         </div>
